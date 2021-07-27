e48d39f655b6d48f7e25b1a71fe35669cd9c94d4
kqiu10 <qiukan97@gmail.com>
2021-07-26 20:54:30 -0400

    used props to manage context

--- react-order/src/components/UI/Modal.js
+++ react-order/src/components/UI/Modal.js
@@ -1,9 +1,9 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 import classes from "./Modal.module.css";
 import ReactDOM from "react-dom";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 const ModalOverlay = (props) => {
   return (
@@ -18,7 +18,7 @@ const portalElement = document.getElementById('overlays')
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
       {ReactDOM.createPortal(<ModalOverlay>{props.children} </ModalOverlay>, portalElement)}
     </Fragment>
   );
