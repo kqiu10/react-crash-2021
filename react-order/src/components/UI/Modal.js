6d9ddef92cd22136cb2d596dc19745fbd43b1a21
kqiu10 <qiukan97@gmail.com>
2021-07-27 18:13:30 -0400

    completed application

--- react-order/src/components/UI/Modal.js
+++ react-order/src/components/UI/Modal.js
@@ -19,13 +19,15 @@ const portalElement = document.getElementById('overlays');
 
 const Modal = (props) => {
   return (
+    <div>
     <Fragment>
       {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
       )}
-    </Fragment>
+      </Fragment>
+      </div>
   );
 };
 
