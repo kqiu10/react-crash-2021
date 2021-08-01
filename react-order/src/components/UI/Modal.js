6b104441afb77dc1228bf9250e5eedfb1704c6ec
kqiu10 <qiukan97@gmail.com>
2021-07-31 23:43:21 -0400

    Update Modal.js

--- react-order/src/components/UI/Modal.js
+++ react-order/src/components/UI/Modal.js
@@ -9,6 +9,7 @@ const Backdrop = (props) => {
 
 const ModalOverlay = (props) => {
   return (
+
     <div className={classes.modal}>
       <div className={classes.content}>{props.children}</div>
     </div>
